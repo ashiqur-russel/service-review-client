0551200ba353148721900d7f4595fe8c61f93028
ashiqurras <ashiqurrussel2@gmail.com>
2022-11-10 17:17:14 +0100

    extra padding and margin fixed for about page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -28,7 +28,7 @@ const About = () => {
         className="container mx-auto h-full relative"
       >
         {/* text & img wrapper */}
-        <div className="flex flex-col lg:flex-row h-full items-center justify-center gap-x-24 text-center lg:text-left lg:pt-16">
+        <div className="flex flex-col lg:flex-row h-full items-center justify-center gap-x-24 text-center lg:text-left lg:pt-5">
           {/* image */}
           <div className="flex-1 max-h-96 lg:max-h-max order-2 lg:order-none overflow-hidden">
             <img src={WomanImg} alt="" />
@@ -39,7 +39,7 @@ const About = () => {
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: "-80%" }}
             transition={transition1}
-            className="flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start"
+            className="flex-1 pt-5 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start"
           >
             <h1 className="h1">About me</h1>
             <p className="mb-12 max-w-sm">
